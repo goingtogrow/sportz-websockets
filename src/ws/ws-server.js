73cc520fe299f37d195b8c1794c88a2cad092501
Artem <artemchuvikin2003@mail.ru>
2026-02-07 12:16:49 +0300

    increment ws heartbit

--- src/ws/ws-server.js
+++ src/ws/ws-server.js
@@ -17,25 +17,28 @@ function broadcast(wss, payload) {
 export function attachWebSocketServer(server) {
     const wss = new WebSocketServer({ server, path: '/ws', maxPayload: 1024 * 1024 })
 
-    wss.on('connection', (socket) => { 
+    wss.on('connection', (ws) => { 
+        ws.isAlive = true;
 
-        socket.isAlive = true;
-        socket.on('pong', () => { socket.isAlive = true });
+        ws.on('pong', () => { 
+            ws.isAlive = true;
+        })
 
-        sendJson(socket, {type: 'welcome'})
-
-        socket.on('error', console.error)
+        sendJson(ws, { type: 'welcome' })
     })
 
     const interval = setInterval(() => {
-        wss.clients.forEach((ws) => { 
-            if (ws.isAlive === false) return ws.terminate();
-            ws.isAlive = false;
-            ws.ping();
-        });
+        for (const ws of wss.clients) { 
+            if (!ws.isAlive) {
+                ws.terminate();
+                continue;
+            }
+            ws.isalive = false;
+            ws.ping;
+        }
     }, 30000);
 
-    ws.on('close', () => clearInterval(interval));
+    wss.on('close', () => clearInterval(interval));
 
     function broadcastMatchCreated(match) {
         broadcast(wss, {type: 'match_created', data: match })
